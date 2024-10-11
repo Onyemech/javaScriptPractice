a0ab0eea36a4c68092da7ba0e11affda93c2df04
Onyemechi Caleb <Onyemechicaleb4@gmail.com>
2024-10-11 16:44:43 +0100

    By his Grace this one must work

---
+++ OneDrive/Documents/Phase_Gate_3/lottery.py
@@ -0,0 +1,17 @@
+numbers = [3, 4, 5]
+user_number = input("Enter a number : ").split()
+
+for counter in range(len(user_number)):
+	
+		if user_number[counter] == numbers[0]:
+			print("You won $2,000")
+			break
+		elif user_number[counter] == numbers[0] and user_number[counter] == numbers[1] :
+			print("You won $2,000")
+			break
+		elif user_number[counter] == numbers[0]:
+			print("You won $2,000")
+			break
+		else:
+			print("You won $2,000")
+			break
