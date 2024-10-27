20a18cbe0d2abe070850d1c60b25ba9203adbe39
Onyemechi Caleb <Onyemechicaleb4@gmail.com>
2024-10-27 22:57:49 +0100

    Done

--- gatOne/MenstralApp.js
+++ gatOne/MenstralApp.js
@@ -1,18 +1,36 @@
 const ps = require("prompt-sync");
 const prompt = ps();
 
-let userName = prompt("Welcome to my menstruation cycle calculator \nPlease Enter your name: ");
-let userAge = prompt("Enter thy age: ");
-let userMonth = prompt("Enter the month in numbers (1-12): ");
+console.log("Welcome to my menstruation cycle calculator");
+
+let userName = prompt("Please Enter your name: ");
+console.log("Hello, " + userName + "!");
+
+let userAge = parseInt(prompt("Enter thy age: "));
+let userMonth = parseInt(prompt("Enter the month in numbers : "));
 
 if (userAge < 8 || userAge > 50) {
-    console.log("Please consult your doctor ");
+	console.log("Please consult your doctor.");
 } else {
-    let cycleLength = prompt("Enter cycle length: ");
-    let lastFlowDate = prompt("Enter last flow day: ");
-    getNextFlowDate(cycleLength, lastFlowDate, userMonth);
+	let cycleLength = parseInt(prompt("Enter cycle length: "));
+        let lastFlowDate = parseInt(prompt("Enter last flow day: "));
+    	getNextFlowDate(cycleLength, lastFlowDate, userMonth);
 }
 
 function getNextFlowDate(cycleLength, lastFlowDate, userMonth) {
-   
+    	let year = new Date().getFullYear();
+    	let lastFlowDateObj = new Date(year, userMonth - 1, lastFlowDate);
+    
+    	let nextMenstruationDate = new Date(lastFlowDateObj);
+    	nextMenstruationDate.setDate(nextMenstruationDate.getDate() + cycleLength);
+    
+    	let ovulationDate = new Date(lastFlowDateObj);
+    	ovulationDate.setDate(ovulationDate.getDate() + Math.floor(cycleLength / 2));
+    
+    	let safePeriodStart = new Date(nextMenstruationDate);
+    	safePeriodStart.setDate(safePeriodStart.getDate() - 18);
+        
+    	console.log("Your next flow date is: " + (nextMenstruationDate.getMonth() + 1) + "-" + nextMenstruationDate.getDate());
+    	console.log("Your ovulation starting date is: " + (ovulationDate.getMonth() + 1) + "-" + ovulationDate.getDate());
+    	console.log("Your safe period starts from: " + (safePeriodStart.getMonth() + 1) + " and ends in 5 days ");
 }
